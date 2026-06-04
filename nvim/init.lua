5fa2e855eef837768cf85a88de9c58fec2aa43ae
Elijah James <elijames2301@gmail.com>
2026-06-04 11:21:02 -0400

    add nvim treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -398,7 +398,12 @@ vim.pack.add({
 	"https://www.github.com/nvim-tree/nvim-tree.lua",
   "https://github.com/ibhagwan/fzf-lua",
   "https://github.com/nvim-mini/mini.nvim",
-  "https://github.com/lewis6991/gitsigns.nvim"
+  "https://github.com/lewis6991/gitsigns.nvim",
+  {
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+	},
 })
 	
 local function packadd(name)
@@ -408,10 +413,62 @@ packadd("fzf-lua")
 packadd("nvim-tree.lua")
 packadd("mini.nvim")
 packadd("gitsigns.nvim")
+packadd("nvim-treesitter")
 
 -- ============================================================================
 -- PLUGIN CONFIGS
 -- ============================================================================
+local setup_treesitter = function()
+	local treesitter = require("nvim-treesitter")
+	treesitter.setup({})
+	local ensure_installed = {
+		"vim",
+		"vimdoc",
+		"rust",
+		"c",
+		"cpp",
+		"go",
+		"html",
+		"css",
+		"javascript",
+		"json",
+		"lua",
+		"markdown",
+		"python",
+		"typescript",
+		"vue",
+		"svelte",
+		"bash",
+    "dart",
+	}
+
+	local config = require("nvim-treesitter.config")
+
+	local already_installed = config.get_installed()
+	local parsers_to_install = {}
+
+	for _, parser in ipairs(ensure_installed) do
+		if not vim.tbl_contains(already_installed, parser) then
+			table.insert(parsers_to_install, parser)
+		end
+	end
+
+	if #parsers_to_install > 0 then
+		treesitter.install(parsers_to_install)
+	end
+
+	local group = vim.api.nvim_create_augroup("TreeSitterConfig", { clear = true })
+	vim.api.nvim_create_autocmd("FileType", {
+		group = group,
+		callback = function(args)
+			if vim.list_contains(config.get_installed(), vim.treesitter.language.get_lang(args.match)) then
+				vim.treesitter.start(args.buf)
+			end
+		end,
+	})
+end
+
+setup_treesitter()
 
 require("nvim-tree").setup({
   view = {
